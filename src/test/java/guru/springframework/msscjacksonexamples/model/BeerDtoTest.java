5a7d986e98dc05429f362f311e27d0de87fbe8d5
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-07-29 21:38:47 +0200

    Testing JsonDeserialize with custom field deserialization

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -20,8 +20,7 @@ class BeerDtoTest extends BaseTest {
 
     @Test
     void testDeserialize() throws IOException {
-        String json = "{\"beerId\":\"d6df1c3f-6d8e-4a9d-84e4-2c17e9b3459e\",\"beerName\":\"Beer Name\",\"beerStyle\":\"Ale\",\"upc\":123123123,\"price\":12.99,\"createdDate\":\"2021-07-29T20:43:46.685066+02:00\",\"lastUpdatedDate\":\"2021-07-29T20:43:46.686437+02:00\"}\n";
-
+        String json = "{\"beerName\":\"Beer Name\",\"beerStyle\":\"Ale\",\"upc\":123123123,\"price\":\"12.99\",\"createdDate\":\"2021-07-29T21:37:21+0200\",\"lastUpdatedDate\":\"2021-07-29T21:37:21.273979+02:00\",\"myLocalDate\":\"20210729\",\"beerId\":\"2ad9855a-2f2d-42bb-aa47-8ff7fd2b54a5\"}\n";
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 
         System.out.println(dto);
